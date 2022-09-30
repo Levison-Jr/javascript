0ff8f5ceaf1bbc056f3cae09b06a334e0f8a46c6
Levison Junior <111018159+Levison-Jr@users.noreply.github.com>
2022-09-30 19:14:01 -0300

    att

--- teste/scripts.js
+++ teste/scripts.js
@@ -3,22 +3,22 @@ $(function() { //Função de layout da tabela
     $(".conteiner").draggable({handle: "#nav"});
 });
 
-var seletorPrincipal_global="";
+var seletorPrincipal_global = "";
 var idRandomGlobal = 1;
 
 function editar(seletorPrincipal) {    
-    var pro = seletorPrincipal.find(".pro").html();
+    var prod = seletorPrincipal.find(".prod").html();
     seletorPrincipal_global = seletorPrincipal;
-    $("#inome").val(pro);
+    $("#inome").val(prod);
     
     $("#bt-cadastro").html('Salvar');
     $( "#tabs" ).tabs({ active: 0 });
 }
 
 function cadastrar() {
-    var pro=$("#inome").val();
+    var prod=$("#inome").val();
 
-    if(pro == "") {
+    if(prod == "") {
         alert("Campo NOME DO PRODUTO inválido!");
         $("#inome").focus();
         return false;
@@ -29,38 +29,43 @@ function cadastrar() {
 
     var addRow = '<tr>'+
                 '<th scope="row" class="id">'+idRandomGlobal+'</th>'+
-                '<td class="pro">'+pro+'</td>'+
+                '<td class="prod">'+prod+'</td>'+
                 '<td>'+dataAtual+'</td>'+
                 '<td>'+horaAtual+'</td>'+
                 '<td><button type="button" class="btn btn-secondary btn-sm bt-editar">Editar</button> <button type="button" class="btn btn-danger btn-sm bt-remover">Remover</button></td>'+
                 '</tr>';
     
-    if(seletorPrincipal_global) { //Conferindo o botão pressionado EDITAR/REMOVER
-         seletorPrincipal_global.find(".pro").html($("#inome").val());
-         seletorPrincipal_global="";
+    if(seletorPrincipal_global) { //Conferindo o botão pressionado EDITAR/CADASTRAR
+         seletorPrincipal_global.find(".prod").html($("#inome").val());
+         seletorPrincipal_global = "";
          $( "#tabs" ).tabs({ active: 1 });
          $("#bt-cadastro").html('Cadastrar');
     } else {
          $("#lista_produtos").append(addRow);
          $("#inome").focus();
          idRandomGlobal++;
-    }                        
+    }
 
     $("#inome").val(""); //Limpa o campo input
     $(".bt-editar").unbind(); //Limpa a fila de listeners
+    $(".bt-remover").unbind();
     
     $(".bt-editar").click(function() { //Listener botão EDITAR
         editar($(this).parent().parent());
     });
 
     $(".bt-remover").click(function() { //Listener botão REMOVER
-        $(this).parent().parent().remove();
+        var itemExcluir = $(this).parent().parent().find(".prod").html();
+        if(confirm('Realmente deseja excluir' + ' ' + itemExcluir + ' ' + '?') == 1) {
+            var trash = '<tr>' + $(this).parent().parent().html() + '</tr>';
+            $("#lista_produtos_trash").append(trash);
+            $(this).parent().parent().remove();
+        }
     });
-
 }
 
 $(document).ready(function() { //Após carregamento (document) => Listener botão CADASTRAR
-    $("#bt-cadastro").click(function(){            
+    $("#bt-cadastro").click(function() {            
         cadastrar();            
     });
 });
