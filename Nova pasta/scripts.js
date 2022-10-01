146a8f8a1ffd782c16196dfc86e3857b54b857f5
Levison Junior <111018159+Levison-Jr@users.noreply.github.com>
2022-10-01 17:58:10 -0300

    testes com arquivo csv

---
+++ Nova pasta/scripts.js
@@ -0,0 +1,39 @@
+function getData(resposta) {
+    var db = [];
+    
+    $.ajax({
+        method: "GET",
+        url: "ProdutosBrumado.csv"
+      })
+        .done(function( msg ) {
+            var dadosGerais = msg.split('\r');
+
+            $.each(dadosGerais, function(index, value) {
+                if(index == 0) {
+                    var ind = value.split(";");
+                }
+                
+                if(index > 0) {
+                    var tes = ["codigo"];
+                    var valores = value.split(";");
+                    db.push({"codigo":valores[0], "Produto":valores[2], "NCM":valores[18]});
+                }
+                
+                if(index == 1) {
+                    console.log(index);
+                    console.log(valores);
+                }
+            })
+
+            resposta(db);
+        // $.each(msg.split("\r"),function(key,d){
+        //
+        //     if(key>0){
+        //     var linha = d.split(";");
+        //      db.push({"codigo":linha[0], "Produto":linha[2], "NCM":linha[18]});
+        //     }
+            
+        //     });
+        //  resposta(db);
+        });
+}
