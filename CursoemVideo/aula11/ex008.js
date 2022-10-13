bc67be078ae1dc11aa45fec963f72e77171e5b98
Levison Junior <111018159+Levison-Jr@users.noreply.github.com>
2022-10-13 14:09:43 -0300

    exercícios aula 11

---
+++ CursoemVideo/aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 70;
+if (vel > 60) {
+    console.log(`Você está à ${vel}Km/h!!!`);
+    console.log(`Velocidade máxima de 60Km/h excedida, MULTADO!`);
+} else {
+    console.log(`Você está à ${vel}Km/h!!!`);
+}
---
+++ CursoemVideo/aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'Brasil';
+if (pais.toLowerCase() == 'brasil') {
+    console.log(`Você é Brasileiro!!!`);
+} else {
+    console.log(`Você é estrangeiro!!!`);
+}
